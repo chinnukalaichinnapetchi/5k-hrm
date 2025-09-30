8ac41e404a6bb1732dc9e8bc5e5cb2eb08cd466c
chinnukalaichinnapetchi <petchi24127@gmail.com>
2025-09-30 10:48:09 +0530

    careCare_changed_5KHRM

--- ios/CarCare/AppDelegate.swift
+++ ios/CarCare/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "CarCare",
+      withModuleName: "5KHRM",
       in: window,
       launchOptions: launchOptions
     )
